fa37a1f8b0a7fdaec1c9d8841297e11a8de6043b
Akshaya <akshayasatheesh@hotmail.co.uk>
2021-05-19 19:04:51 +0100

    Created post request function for creating person

--- people.py
+++ people.py
@@ -6,6 +6,9 @@ PEOPLE collection
 # Import datetime module - SYSTEM moule
 from datetime import datetime
 
+# 3rd party modules
+from flask import make_response, abort
+
 # Function returning a string of current time
 def get_timestamp(): 
   return datetime.now().strftime(("%Y-%m-%d %H:%M:%S"))
@@ -38,3 +41,52 @@ def read_all():
 
   # Create the list of people from our data
   return [PEOPLE[key] for key in sorted(PEOPLE.keys())]
+
+def read_one(lname):
+  """
+  This function responds to a request for /api/people/{lname}
+  with one matching person from people
+
+  :param lname:   last name of person to find
+  :return:        person matching last name
+  """
+  # Does this person exist in people?
+  if lname in PEOPLE:
+    person = PEOPLE.get(lname)
+
+  # Otherwise, nope, not found
+  else:
+    abort(
+      404, "Person with last name {lname} not found".format(lname=lname)
+    )
+  
+  return person
+
+def create(person):
+  """
+  This function creates a new person in the people structure
+  based on the passed in person data
+
+  :param person:    person to create in people structure
+  :return:          201 on success, 406 on person exists
+  """
+  lname = person.get("lname", None)
+  fname = person.get("fname", None)
+
+  # Does this person exist already?
+  if lname not in PEOPLE and lname is not None:
+    PEOPLE[lname] = {
+      "lname": lname,
+      "fname": fname,
+      "timestamp": get_timestamp(),
+    }
+    return make_response(
+      "{lname} successfully created".format(lname = lname), 201
+    )
+
+  # Otherwise, they exist, that's an error
+  else:
+    abort(
+      406, 
+      "Person with last name {lname} already exists".format(lname = lname)
+    )
